4e217b8fe8a3061c9985ef4d311165a508ed3a26
abbosrakhmonov <abbos.raxmonov.2001@gmail.com>
2024-07-02 10:25:05 +0500

    hello1

--- src/index.js
+++ src/index.js
@@ -76,6 +76,14 @@ app.listen(port, () => {
 
 start();
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
 // module.exports = async (req, res) => {
 //   try {
 //     if (req.method === "POST") {
