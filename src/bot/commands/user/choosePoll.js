7acd9bb0c14aef4314c41f61292ea89bf6083f7f
abbosrakhmonov <abbos.raxmonov.2001@gmail.com>
2024-07-02 18:02:33 +0500

    alyo

--- src/bot/commands/user/choosePoll.js
+++ src/bot/commands/user/choosePoll.js
@@ -6,26 +6,27 @@ const contact = require("../requiredContact");
 const choosePoll = async (ctx) => {
   try {
     const pollId = ctx.callbackQuery.data.split("_")[1];
-    const [poll, user] = await Promise.all([
-      Poll.findById(pollId).lean(),
-      User.findOne({ telegramId: ctx.from.id })
-    ]);
-
+    const poll = await Poll.findById(pollId).lean();
     if (!poll) {
       return await ctx.answerCbQuery("Сўровнома топилмади");
     }
+    const user = await User.findOne({ telegramId: ctx.from.id });
+
+    const existingVote = user.votes.find((vote) => vote.pollId === pollId);
 
     if (!user) {
       await ctx.answerCbQuery("Сиз рўйхатдан ўтмагансиз");
       return await contact(ctx);
     }
 
-    const existingVote = user.votes.find((vote) => vote.pollId === pollId);
-
-    const buttons = poll.options.map((option, index) => Markup.button.callback(
-      `(${option.votes}) ${option.text} ${existingVote && existingVote.optionIndex === index ? " ✅" : ""}`,
-      `vote_${pollId}_${index}`,
-    ));
+    let buttons = poll.options.map((option, index) => [
+      Markup.button.callback(
+        `(${option.votes}) ${option.text} ${
+          existingVote && existingVote.optionIndex === index ? " ✅" : ""
+        }`,
+        `vote_${pollId}_${index}`,
+      ),
+    ]);
 
     try {
       await ctx.telegram.copyMessage(
